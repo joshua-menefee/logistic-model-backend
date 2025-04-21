a5dc66559706e84bb2132c27dfc7721b2396a0b6
j-caine <jmenefe3@students.kennesaw.edu>
2025-04-21 19:37:36 -0400

    Fix CORS origin to match GitHub Pages

--- src/main/java/logisitic_model/AdoptionController.java
+++ src/main/java/logisitic_model/AdoptionController.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "https://joshua-menefee.github.io/logistic-model-frontend/")
+@CrossOrigin(origins = "https://joshua-menefee.github.io")
 @RestController
 public class AdoptionController {
     @GetMapping("/simulate")
